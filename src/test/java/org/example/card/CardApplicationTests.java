9a8eb661b9aa34f32910b8c68f742e19f1f40b68
duwniy <execmyaccaunt@gmail.com>
2025-12-23 23:34:34 +0500

    first commit

---
+++ src/test/java/org/example/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
